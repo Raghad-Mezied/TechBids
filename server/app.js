df73278ec8f2c819c25eaeeec8ffa06f6b9912b0
aaamra <dev.aaamra@gmail.com>
2021-11-23 09:41:43 +0200

    create server

---
+++ server/app.js
@@ -0,0 +1,22 @@
+require('env2')('.env');
+const { join } = require('path');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+// const router = require('./routes');
+
+const app = express();
+
+app.set('PORT', process.env.PORT || 8000);
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use('/api', router);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(join(__dirname, '..', 'client', 'build')));
+  app.get('*', (req, res) => {
+    res.sendFile(join(__dirname, '..', 'client', 'build', 'index.html'));
+  });
+}
+
+module.exports = app;
