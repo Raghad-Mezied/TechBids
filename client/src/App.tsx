6917b9f8f2d05267060b94441ccbfc2aa715f4b9
rawand1998 <rawandgaradh@gmail.com>
2021-11-26 20:31:51 +0200

    fix eslint error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import './App.css';
-import Footer from './Components/Common/Footer';
 
 const App : React.FC = () => (
   <div className="App">
     hello world
-    <Footer />
-
   </div>
 );
 
