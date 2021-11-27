75f2a7bdf041ff40655713d1b939a943737ec799
AhemdAboAbadi <ahmed.free.py@gmail.com>
2021-11-27 14:30:42 +0200

    edit schema validation

--- server/src/controllers/validations/signInValidation.js
+++ server/src/controllers/validations/signInValidation.js
@@ -2,7 +2,7 @@ const joi = require('joi');
 
 const signInValidation = joi.object({
   email: joi.string().email().required(),
-  password: joi.string().required(),
+  password: joi.string().alphanum().min(6).required(),
 });
 
 module.exports = { signInValidation };
