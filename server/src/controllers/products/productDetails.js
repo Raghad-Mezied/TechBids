e30fddab61966c0bcf7bf04446afa012c27ec29d
rawand1998 <rawandgaradh@gmail.com>
2021-11-29 18:02:41 +0200

    testing product details route

--- server/src/controllers/products/productDetails.js
+++ server/src/controllers/products/productDetails.js
@@ -1,18 +1,17 @@
 const { Product } = require('../../models/index');
 const boomify = require('../../utils/index');
 
-const productDetails = async (req, res) => {
+const productDetails = async (req, res, next) => {
   const { id } = req.params;
 
   try {
     if (id <= 0) { throw boomify(400, 'Bad Request', 'Bad Request'); }
     const data = await Product.findByPk(id);
-
     res.json({
       data,
     });
   } catch (err) {
-    console.log(err);
+    next(err);
   }
 };
 
