3e0c07ca87583a81691c5bc3761464d8c8cb958f
Raghad-Mezied <rmezied3@gmail.com>
2021-11-30 17:02:24 +0200

    create auth route, handle and test its controller

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,11 +1,16 @@
 const router = require('express').Router();
 
 const {
-  serverError, clientError, signIn, handleAddUser,
+  serverError,
+  clientError,
+  signIn,
+  handleAddUser,
+  handleAuthUser,
 } = require('../controllers');
+const { isAuth } = require('../controllers/middlewares');
 
+router.get('/auth/user', isAuth, handleAuthUser);
 router.post('/signIn', signIn);
-
 router.post('/signup', handleAddUser);
 router.use(clientError);
 router.use(serverError);
