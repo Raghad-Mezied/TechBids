71fe1d6a42202b59bb69fb036e64d1a61ccb5093
rawand1998 <rawandgaradh@gmail.com>
2021-11-30 12:51:13 +0200

    create controller

--- server/src/controllers/auctions/getAuction.js
+++ server/src/controllers/auctions/getAuction.js
@@ -1,4 +1,18 @@
-const getAuction = () => {
+const { Auction } = require('../../models/index');
+const boomify = require('../../utils/index');
 
+const getAuction = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    if (id <= 0) { throw boomify(400, 'Bad Request', 'Bad Request'); }
+    const data = await Auction.findByPk(id);
+    res.json({
+      data,
+    });
+  } catch (err) {
+    next(err);
+  }
 };
+
 module.exports = { getAuction };
