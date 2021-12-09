7b6d2ff2a1817553786c1dcbdb34baaadf4914ec
asmaaamin <eng.asmaa.a.an@gmail.com>
2021-12-09 20:40:56 +0200

    edit bids cards style

--- client/src/pages/Bids/index.tsx
+++ client/src/pages/Bids/index.tsx
@@ -89,7 +89,7 @@ const Bids : React.FC = () => {
           <BidsStatusCheckBox setPage={setPage} status={status} setStatus={setStatus} />
         </div>
         <div className="side-container">
-          <div className="card-container">
+          <div className="card-container2">
             {isLoading && Array.from(new Array(3)).map(() => <Skeleton variant="rectangular" width="30%" height={300} />)}
             {!isLoading && (products.length ? products.map((item:itemProp) => (
               <ProductCard
